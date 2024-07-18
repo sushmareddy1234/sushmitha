114209e058599f1eef6ff660dd26beb7602d9280
unknown <sushmitha>
2024-07-18 14:22:43 +0530

    first commit

---
+++ p2.py
@@ -0,0 +1,1 @@
+https://github.com/sushmareddy1234/DevOps_2.git
---
+++ pg2.py
@@ -0,0 +1,11 @@
+nums = (1, 2, 3, 4)
+
+sum_nums = 0
+
+for num in nums:
+    sum_nums = sum_nums + num
+
+print(f'Sum of numbers is {sum_nums}')
+
+# Output
+# Sum of numbers is 10
