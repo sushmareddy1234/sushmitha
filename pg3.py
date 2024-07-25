f14b3bef25439ba86aa5bf260e7abc94a94b64d9
unknown <sushmitha>
2024-07-25 13:39:55 +0530

    commiting the changes to local repo

---
+++ pg3.py
@@ -0,0 +1,9 @@
+# Program to demonstrate a for loop
+
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Iterate over each element in the list using a for loop
+for num in numbers:
+    # Print each number multiplied by 2
+    print(num * 2)
